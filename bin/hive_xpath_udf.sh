152911bf770ef7d23c19acce4320c251a6fe8de3
Shane Kumpf <sakserv@sakserv.org>
2015-03-03 18:51:05 -0700

    fix drop table statement

--- bin/hive_xpath_udf.sh
+++ bin/hive_xpath_udf.sh
@@ -4,5 +4,5 @@
 . /tmp/hive-xml-examples/conf/env.cfg
 
 echo -e "\n###  Creating table $HIVE_XPATH_TABLE_NAME"
-hive -e "DROP TABLE $HIVE_XPATH_TABLE_NAME IF EXISTS;"
+hive -e "DROP TABLE IF EXISTS $HIVE_XPATH_TABLE_NAME;"
 hive -e 'CREATE EXTERNAL TABLE $HIVE_XPATH_TABLE_NAME (string xmldoc) LOCATION \'$SAMPLE_DATA_DIR\'';
