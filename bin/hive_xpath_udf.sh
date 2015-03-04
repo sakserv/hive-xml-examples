ad95d71b80008da7eb6666189b227da40de2f830
Shane Kumpf <sakserv@sakserv.org>
2015-03-03 18:45:50 -0700

    fix quoting

--- bin/hive_xpath_udf.sh
+++ bin/hive_xpath_udf.sh
@@ -5,4 +5,4 @@
 
 echo -e "\n###  Creating table $HIVE_XPATH_TABLE_NAME"
 hive -e "DROP TABLE $HIVE_XPATH_TABLE_NAME IF EXISTS;"
-hive -e "CREATE TABLE $HIVE_XPATH_TABLE_NAME (string xmldoc) LOCATION ''$SAMPLE_DATA_DIR''";
+hive -e "CREATE TABLE $HIVE_XPATH_TABLE_NAME (string xmldoc) LOCATION '$SAMPLE_DATA_DIR'";
