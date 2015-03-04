d0418593a883bedf633a3f5d53882e1df89d343b
Shane Kumpf <sakserv@sakserv.org>
2015-03-03 18:36:05 -0700

    updated readme and added intellij bits

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # hive-xml-examples
 
 A collection of examples of working with XML with Hive
+
+Setup:
+------
+
+* Clone the project
+```
+cd /tmp && git clone https://github.com/sakserv/hive-xml-examples.git
+```
+
+* Ingest the sample data
+```
+cd /tmp/hive-xml-examples && bash -x bin/ingest_data.sh
+```
---
+++ bin/ingest_data.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+. /tmp/hive-xml-examples/conf/env.cfg
+
+# Create the base dir in HDFS
+echo -e "\n###  Creating $HDFS_BASE_DIR in HDFS"
+if hdfs dfs -test $HDFS_BASE_DIR; then
+  hdfs dfs -rm -r $HDFS_BASE_DIR
+fi
+hdfs dfs -mkdir $HDFS_BASE_DIR
+
+# Ingest the data
+echo -e "\n### Ingesting the sample data"
+hdfs dfs -put $SAMPLE_DATA_DIR/auctions.xml $HDFS_BASE_DIR
+hdfs dfs -put $SAMPLE_DATA_DIR/books.xml $HDFS_BASE_DIR
